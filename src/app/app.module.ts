16529faa9d5dd850f308256a18d333f40b12607d
Joe Monaco <jmonaco0506@gmail.com>
2020-04-20 15:37:02 -0400

    player search done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from "@angular/common/http";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiServiceService } from './services/api-service.service';
+import { PlayerSearchComponent } from './components/player-search/player-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
